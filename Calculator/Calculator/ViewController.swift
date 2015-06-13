559582f99952fa9edb1ad36249b9e42ab5999686
eric yu <yulin.jay@gmail.com>
2015-06-13 04:40:14 -0700

    Lecture 2 commit

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -29,7 +29,77 @@ class ViewController: UIViewController
         
         
     }
+    var numberStack = Array<Double>()
 
+    @IBAction func enter()
+    {
+        numberStack.append(displayValue)
+        isFirstDigit = true
+        println("numberStack : \(numberStack)" )
+    }
+    
+    var displayValue : Double{
+        get{
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set{
+            display.text = "\(newValue)"
+        }
+    }
+    
+    @IBAction func operate(sender: UIButton)
+    {
+        let operation =  sender.currentTitle!
+        
+        if(!isFirstDigit)
+        {
+            enter()
+        }
+        
+        switch operation
+        {
+//        case "+" : performOperation({(op1 : Double, op2 : Double) -> Double in            
+//            return op1+op2
+//            })
+
+//        case "+" : performOperation({(op1, op2) in op1+op2})
+        case "+" : performOperation({$0 + $1 })
+            
+            
+        case "−" : performOperation({$1 - $0 })
+        case "×" : performOperation({$0 * $1 })
+        case "÷" : performOperation({$1 / $0 })
+            
+        case "√" : performOperation2({sqrt($0)})
+            
+        default : break
+        }
+    }
+    
+    func performOperation(operation: (Double,Double)->Double)
+    {
+        if(numberStack.count >= 2)
+        {
+            displayValue = operation(numberStack.removeLast() , numberStack.removeLast())
+            enter()
+        }
+    }
+    
+//    func plus(op1 : Double, op2 : Double) -> Double
+//    {
+//        return op1+op2
+//    }
+    func performOperation2(operation: Double -> Double)
+    {
+        if(numberStack.count >= 1)
+        {
+            displayValue = operation(numberStack.removeLast())
+            enter()
+        }
+    }
+    
+    
+    
 
 }
 
